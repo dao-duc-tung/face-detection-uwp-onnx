3498b0fb750f028247f60ced27aee33ee4dca8ee
tungdao17 <tungdao17@gmail.com>
2021-03-06 19:04:52 +0800

    Add functionality to init camera, read frame

--- src/ViewModels/MainPageViewModel.cs
+++ src/ViewModels/MainPageViewModel.cs
@@ -11,16 +11,31 @@ using Windows.Storage;
 using Windows.Storage.Streams;
 using FaceDetection.FaceDetector;
 using System.Diagnostics;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Devices.Enumeration;
+using Windows.Media.MediaProperties;
+using System.Threading;
+using System.IO;
 
 namespace FaceDetection.ViewModels
 {
     public class MainPageViewModel : BaseNotifyPropertyChanged
     {
         private FrameModel _frameModel = new FrameModel();
-        private IFaceDetector _faceDetector;
+
+        private bool _isInitialized;
+        public bool IsPreviewing { get; set; }
+        public MediaCapture MediaCapture { get; set; }
+
+        private int _processingFlag;
+        private MediaFrameReader _frameReader;
+
+
         // TODO: Create Config to load modelFileName
         private string _modelFileName = "version-RFB-320.onnx";
 
+        private IFaceDetector _faceDetector;
         private bool _isFaceDetectionEnabled = false;
         public bool IsFaceDetectionEnabled {
             get => this._isFaceDetectionEnabled;
@@ -56,6 +71,104 @@ namespace FaceDetection.ViewModels
             {
                 PerformFaceDetection();
             }
+        }
+
+        public void OnFrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+        {
+            if (Interlocked.CompareExchange(ref _processingFlag, 1, 0) == 0)
+            {
+                try
+                {
+                    using (var frame = sender.TryAcquireLatestFrame())
+                    using (var bmp = frame?.VideoMediaFrame?.SoftwareBitmap)
+                    {
+                        if (bmp != null)
+                        {
+                            this._frameModel.SoftwareBitmap = bmp;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Interlocked.Exchange(ref _processingFlag, 0);
+                }
+            }
+        }
+
+        public async Task InitCameraAsync()
+        {
+            if (!this._isInitialized)
+            {
+                await InitMediaCaptureAsync();
+            }
+            if (!this._isInitialized) return;
+
+            await InitFrameReaderAsync();
+        }
+
+        public async Task StartPreviewAsync()
+        {
+            try
+            {
+                await this.MediaCapture.StartPreviewAsync();
+                this.IsPreviewing = true;
+            }
+            catch (FileLoadException)
+            {
+                Debug.WriteLine("Another app has exclusive access");
+            }
+        }
+
+        public async Task StopPreviewAsync()
+        {
+            this.IsPreviewing = false;
+            await this.MediaCapture.StopPreviewAsync();
+            await this._frameReader.StopAsync();
+        }
+
+        private async Task InitMediaCaptureAsync()
+        {
+            var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Front);
+            if (cameraDevice == null)
+            {
+                Debug.WriteLine("No camera device found");
+                return;
+            }
+
+            // Set Memory Preference to CPU to guarantee SoftwareBitmap property is non-null
+            // Otherwise use Direct3DSurface property
+            // https://docs.microsoft.com/en-us/uwp/api/windows.media.capture.frames.videomediaframe.softwarebitmap?view=winrt-19041
+            this.MediaCapture = new MediaCapture();
+            var settings = new MediaCaptureInitializationSettings
+            {
+                VideoDeviceId = cameraDevice.Id, MemoryPreference = MediaCaptureMemoryPreference.Cpu
+            };
+
+            try
+            {
+                await this.MediaCapture.InitializeAsync(settings);
+                this._isInitialized = true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Debug.WriteLine("The app was denied access to the camera");
+            }
+        }
+
+        private async Task InitFrameReaderAsync()
+        {
+            var frameSource = this.MediaCapture.FrameSources.Where(
+                source => source.Value.Info.SourceKind == MediaFrameSourceKind.Color)
+                .First();
+            this._frameReader = await this.MediaCapture.CreateFrameReaderAsync(frameSource.Value, MediaEncodingSubtypes.Rgb32);
+                
+            // Setup handler for frames
+            this._frameReader.FrameArrived += OnFrameArrived;
+            await this._frameReader.StartAsync();    
         }
 
         private async void PerformFaceDetection()
@@ -115,6 +228,13 @@ namespace FaceDetection.ViewModels
             BitmapDecoder decoder = await BitmapDecoder.CreateAsync(fileStream);
             SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore);
             this._frameModel.SoftwareBitmap = softwareBitmap;
+        }
+
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+            return (desiredDevice == null) ? allVideoDevices.FirstOrDefault() : desiredDevice;
         }
     }
 }
