e7393d398c4a39be9734670d1b8f1d5bf0888052
tungdao17 <tungdao17@gmail.com>
2021-03-06 15:43:40 +0800

    Add FaceBoundingBox

---
+++ src/Utils/FaceBoundingBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetection.Utils
+{
+    public class FaceBoundingBox : CornerBoundingBox
+    {
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+    }
+}
