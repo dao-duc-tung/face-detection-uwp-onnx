6295c17c408e1a0ae6354e208d132eac1820d391
tungdao17 <tungdao17@gmail.com>
2021-03-08 03:02:44 +0800

    Update distance estimator config

--- src/DistanceEstimator/FocalLengthDistanceEstimator.cs
+++ src/DistanceEstimator/FocalLengthDistanceEstimator.cs
@@ -5,19 +5,17 @@ namespace FaceDetection.DistanceEstimator
     public class FocalLengthDistanceEstimator
     {
         private FocalLengthDistanceEstimatorConfig _config;
-        private float _fixedFaceHeightCM;
         private float _focalLength;
 
         public FocalLengthDistanceEstimator(FocalLengthDistanceEstimatorConfig config)
         {
             _config = config;
-            _fixedFaceHeightCM = _config.FixedFaceHeightCM;
-            _focalLength = _config.FixedFaceCameraDistanceCM * _config.FixedFaceHeightPixel / _fixedFaceHeightCM;
+            _focalLength = _config.FixedFaceCameraDistanceCM * _config.FixedFaceHeightPixel / _config.FixedFaceHeightCM;
         }
 
         public float ComputeDistance(CornerBoundingBox bb)
         {
-            return _focalLength * _fixedFaceHeightCM / bb.Height;
+            return _focalLength * _config.FixedFaceHeightCM / bb.Height;
         }
     }
 }
