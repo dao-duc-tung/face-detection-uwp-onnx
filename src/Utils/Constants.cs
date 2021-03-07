103a7faf09060a5aa00a5ab3380360dc2f0225c2
tungdao17 <tungdao17@gmail.com>
2021-03-08 03:02:31 +0800

    Add MainConfig

--- src/Utils/Constants.cs
+++ src/Utils/Constants.cs
@@ -9,12 +9,14 @@ namespace FaceDetection.Utils
 {
     public static class ConfigName
     {
+        public static readonly string Main = "Main";
         public static readonly string UltraFaceDetector = "UltraFaceDetector";
         public static readonly string FocalLengthDistanceEstimator = "FocalLengthDistanceEstimator";
     }
 
     public static class ConfigLocalPath
     {
+        public static readonly string Main = "Assets/MainConfig.json";
         public static readonly string UltraFaceDetector = "Assets/UltraFaceDetectorConfig.json";
         public static readonly string FocalLengthDistanceEstimator = "Assets/FocalLengthDistanceEstimatorConfig.json";
     }
