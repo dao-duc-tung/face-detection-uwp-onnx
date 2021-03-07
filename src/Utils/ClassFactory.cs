925b781b350467731b15be69980a3302aad176dc
tungdao17 <tungdao17@gmail.com>
2021-03-07 15:24:43 +0800

    Add Singleton and SingletonNotifyPropertyChanged classes

---
+++ src/Utils/ClassFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetection.Utils
+{
+    public class Singleton<T> where T : class
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => CreateInstanceOfT());
+        private static T CreateInstanceOfT() => Activator.CreateInstance(typeof(T), true) as T;
+        public static T Instance { get => _instance.Value; }
+    }
+
+    public class SingletonNotifyPropertyChanged<T> : BaseNotifyPropertyChanged where T : class
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => CreateInstanceOfT());
+        private static T CreateInstanceOfT() => Activator.CreateInstance(typeof(T), true) as T;
+        public static T Instance { get => _instance.Value; }
+    }
+}
