dd93ed081e05f309f1e27484fa7a0fefcb7a5b95
tungdao17 <tungdao17@gmail.com>
2021-03-10 22:17:59 +0800

    Add unit test for 2 UltraFaceDetectors

---
+++ UnitTest/TestFixture.cs
@@ -0,0 +1,32 @@
+﻿using FaceDetection.DistanceEstimator;
+using FaceDetection.FaceDetector;
+using FaceDetection.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class TestFixture : IDisposable
+    {
+        public AppConfig AppConfig;
+
+        public TestFixture()
+        {
+            Task.Run(LoadAppConfig).Wait();
+        }
+
+        private async void LoadAppConfig()
+        {
+            await FileUtils.ReadConfigFile<UltraFaceDetectorConfig>(ConfigName.UltraFaceDetector, ConfigLocalPath.UltraFaceDetector);
+            await FileUtils.ReadConfigFile<FocalLengthDistanceEstimatorConfig>(ConfigName.FocalLengthDistanceEstimator, ConfigLocalPath.FocalLengthDistanceEstimator);
+            AppConfig = AppConfig.Instance;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
