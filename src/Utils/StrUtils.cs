e2c269a6d75bd0d56816d2e37e59013abdf67fa6
tungdao17 <tungdao17@gmail.com>
2021-03-10 22:05:29 +0800

    Add StrUtils

---
+++ src/Utils/StrUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetection.Utils
+{
+    public class StrUtils
+    {
+        public static string RdFloat(float number, int nDigit = 2)
+        {
+            if (nDigit < 0) return number.ToString();
+            return number.ToString($"n{nDigit}");
+        }
+
+        public static string RdDouble(double number, int nDigit = 2)
+        {
+            if (nDigit < 0) return number.ToString();
+            return number.ToString($"n{nDigit}");
+        }
+    }
+}
